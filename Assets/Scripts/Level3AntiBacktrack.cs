5a262f0d8363bb6452192556888858dfb4bc0d1e
Odinfrost <kristofferpiper@yahoo.com>
2015-11-18 04:34:51 +0100

    Level 3 Anti-backtrack
    Makes it impossible to get back into level 2 after triggering the
    antibacktrack script

---
+++ Assets/Scripts/Level3AntiBacktrack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level3AntiBacktrack : MonoBehaviour {
+
+    private Vector3 slideVector;
+
+    // Use this for initialization
+    void Start () {
+
+        slideVector = new Vector3(this.transform.position.x, this.transform.position.y- 26f, this.transform.position.z);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if (GameObject.Find("L2Deleter").GetComponent<ExitPoint>().HasEntered)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, slideVector, 0.52f * Time.deltaTime);
+        }
+
+    }
+}
