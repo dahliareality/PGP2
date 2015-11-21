9a72c22bb0fbc2d4ba545c96214a16acb64eebb4
Odinfrost <kristofferpiper@yahoo.com>
2015-11-21 20:09:03 +0100

    Skip level 2 fix
    Made some minor fixes, also, please disable the player in "Level1Mover"
    when wanting to skip.
    Also removed the second "-using UnityEngine" from AnimalAI

--- Assets/Scripts/Level1Mover.cs
+++ Assets/Scripts/Level1Mover.cs
@@ -6,27 +6,31 @@ public class Level1Mover : MonoBehaviour {
     private LoadLevel3Skip2 moveIt;
     private GameObject childObject;
     private GameObject parentObject;
+    //public GameObject player1;
 
 	// Use this for initialization
 	void Start () {
 
         moveIt = GameObject.Find("LoadLevel3").GetComponent<LoadLevel3Skip2>();
-        childObject = GameObject.FindGameObjectWithTag("Player");
         parentObject = GameObject.Find("PlayerHolder");
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //skips level 2
         if (moveIt.skipLvl2 == true)
         {
             GameObject.Find("Level1Mover").SetActive(false);
+            childObject = GameObject.FindGameObjectWithTag("Player");
             childObject.transform.parent = parentObject.transform;
         }
+        //does not skip level 2
         else
         {
             GameObject.Find("Level1Mover (1)").SetActive(false);
+            childObject = GameObject.FindGameObjectWithTag("Player");
             childObject.transform.parent = parentObject.transform;
         }
-	}
+    }
 }
