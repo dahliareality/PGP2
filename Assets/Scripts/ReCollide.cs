d5052008ad3741acd8e748237168f277a1f61a4b
Odinfrost <kristofferpiper@yahoo.com>
2015-11-21 16:48:58 +0100

    Gate of Ishtar Fixes
    Plays animation, deletes boulder

---
+++ Assets/Scripts/ReCollide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReCollide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GameObject.Find("Colls").GetComponent<ExitPoint>().HasEntered)
+        {
+            Debug.Log("hai");
+            for (int i = 1; i < 251; i++)
+            {
+                GameObject.Find("Box006_Part_" + i).GetComponent<Collider>().enabled = true;
+            }
+        }
+
+    }
+}
