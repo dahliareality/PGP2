605fccb71d269fdfb05aed8fda67b5597feaed7c
Jannik Vilhelm Reffstrup <jannik.reffstrup@gmail.com>
2015-11-22 23:04:25 +0100

    Jannik - Crittle + Cliff wall surroundings level2

--- Assets/Scripts/AnimalAI.cs
+++ Assets/Scripts/AnimalAI.cs
@@ -6,21 +6,26 @@ public class AnimalAI : MonoBehaviour {
 	private float distanceToPlayer;
 	private Vector3 vectorTowardsPlayer;
 	private Vector3 vectorAwayFromPlayer;
-	private float moveSpeed;
+	private float moveSpeed = 15;
+	private float maxSpeed = 2;
 	private float triggerDist = 3.0f;
 	private float scaredTime = 3.0f;
 	
 	public bool isScared;
 	public bool hasRunAway = true;
 	private float timer;
-	
+
+	private Rigidbody rigidbodyR;
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigidbodyR = GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		
 		vectorTowardsPlayer = this.gameObject.transform.position - GameObject.FindWithTag ("Player").transform.position;
 		distanceToPlayer = vectorTowardsPlayer.magnitude;
@@ -31,15 +36,29 @@ public class AnimalAI : MonoBehaviour {
 		}
 		
 		if(isScared){
+
+			animator.SetBool("fleeing",true);
+
 			timer += Time.deltaTime;
-			transform.position += vectorAwayFromPlayer.normalized/30f;
+
 			hasRunAway = false;
-			if (timer >= scaredTime){
+
+			if (timer >= scaredTime)
+			{
 				hasRunAway = true;
 				isScared = false;
 				timer = 0f;
 			}
-			transform.right = vectorAwayFromPlayer;
+
+			vectorAwayFromPlayer.Normalize();
+
+			Vector3 directionToGo = new Vector3(vectorAwayFromPlayer.x, 0, vectorAwayFromPlayer.z);
+
+			transform.right  = Vector3.Lerp(transform.right, directionToGo, 0.1f);
+			transform.position += new Vector3(vectorAwayFromPlayer.x, 0, vectorAwayFromPlayer.z)/30f;
+//			rigidbodyR.AddForce(new Vector3(vectorAwayFromPlayer.x, 0, vectorAwayFromPlayer.z)*moveSpeed);
 		}
+		else
+			animator.SetBool("fleeing",false);
 	}
 }
