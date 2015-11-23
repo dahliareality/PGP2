62504e5a56fd41da7a5637f3524a805db82d95ba
Prawnkus <prawnkus@gmail.com>
2015-11-23 05:10:12 +0100

    Credit scene added in asset folder
    Plays credits and music

---
+++ Assets/Scripts/VidPlay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VidPlay : MonoBehaviour {
+	
+	void Update () {
+		
+		Renderer r = GetComponent<Renderer>();
+		MovieTexture movie = (MovieTexture)r.material.mainTexture;
+		
+		movie.Play();
+	}
+}
