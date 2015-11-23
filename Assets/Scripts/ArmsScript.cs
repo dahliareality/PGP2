088b3871473e52217e6a3f6e03a236d6807d461d
Jannik Vilhelm Reffstrup <jannik.reffstrup@gmail.com>
2015-11-23 03:19:28 +0100

    Revert "Jannik- Problems with pick up items + Animal AI FIX"
    This reverts commit bfa0f4649f99fe3801abd92c253ec541e56ea28b.

--- Assets/Scripts/ArmsScript.cs
+++ Assets/Scripts/ArmsScript.cs
@@ -170,12 +170,6 @@ public class ArmsScript : MonoBehaviour {
 			else if (!inventory.HasBagOpen && !isCarryingItem)
 				GameObject.FindGameObjectWithTag("Animator").GetComponent<IKControl>().movement = 0;
 		}
-
-		if (isCarryingItem)
-		if (objInRightArm.transform.position != objectSpaceInHand.transform.position)
-			objInRightArm.transform.position = objectSpaceInHand.transform.position;
-
-
 		
 	}
 	
@@ -204,13 +198,10 @@ public class ArmsScript : MonoBehaviour {
 			obj.transform.localScale = new Vector3(70.0f, 70.0f, 70.0f);
 		}
 		playerObject.GetComponent<RayCast>().setStoredPickUpItem(obj);
-//		if (!obj.GetComponent<BoxCollider>().isTrigger)
-//		{
-//			obj.GetComponent<BoxCollider>().isTrigger = true;
-//		}
-		obj.GetComponent<BoxCollider> ().enabled = false;
-		obj.GetComponent<Rigidbody>().useGravity = false;
-
+		if (!obj.GetComponent<BoxCollider>().isTrigger)
+		{
+			obj.GetComponent<BoxCollider>().isTrigger = true;
+		}
 		//Plays sound
 		if (isFromBackpack == true){
 			retreiveSound.GetComponent<SECTR_PointSource>().Play ();
@@ -227,8 +218,6 @@ public class ArmsScript : MonoBehaviour {
 		objInRightArm = null;
 		item.transform.parent = null;
 		item.layer = 0;
-		item.GetComponent<BoxCollider> ().enabled = true;
-		item.GetComponent<Rigidbody>().useGravity = true;
 	}
 	
 	public void DropItem(GameObject item)
@@ -238,8 +227,6 @@ public class ArmsScript : MonoBehaviour {
 		item.transform.parent = null;
 		item.transform.rotation = Quaternion.identity;
 		item.layer = 0;
-		item.GetComponent<BoxCollider> ().enabled = true;
-		item.GetComponent<Rigidbody>().useGravity = true;
 		// Checks for Pickable Scripts
 		if (item.GetComponent<Pickable>() != null)
 		{
