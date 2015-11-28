adafbb9ee60552e8712fb8ed5108b35fd69eb9a2
Theis Berthelsen <theis.berthelsen@gmail.com>
2015-11-29 00:13:36 +0100

    Start Menu added.

---
+++ Assets/Scripts/MenuMouseHandling.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMouseHandling : MonoBehaviour {
+
+	
+	void OnMouseDown()
+	{	
+				//On pressed, starts loading the Endless Runner game scene
+
+				Application.LoadLevel(1);
+		
+	}
+	
+	void Update()
+	{
+		if(Input.anyKeyDown|| Input.GetButton("PS4_X") )
+            
+		{
+			
+				Application.LoadLevel(1);
+		}
+		
+	}
+	
+}
---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class PlayMusic : MonoBehaviour
+{
+
+    public AudioClip[] musicClips;                 
+
+
+    private AudioSource musicSource;                
+
+    void Awake()
+    {
+       
+        musicSource = GetComponent<AudioSource>();
+    }
+
+    //Used if running the game in a single scene, takes an integer music source allowing you to choose a clip by number and play.
+    public void PlaySelectedMusic(int musicChoice)
+    {
+        //Play the music clip at the array index musicChoice
+        musicSource.clip = musicClips[musicChoice];
+    }
+}
