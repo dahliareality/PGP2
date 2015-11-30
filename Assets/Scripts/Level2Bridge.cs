1b7a8ca0acfc23cbd94f33fcc00c1ca9fdf6f4b0
Peter Rossing <hprossing@gmail.com>
2015-11-30 12:41:59 +0100

    Lvl 2 bridge/coin update
    Increased the size of the coins collider.
    Added invisible block in front of the bridge. Removing said block when
    bridge is up.

--- Assets/Scripts/Level2Bridge.cs
+++ Assets/Scripts/Level2Bridge.cs
@@ -28,6 +28,7 @@ public class Level2Bridge : MonoBehaviour
         parenting = GameObject.Find("Lv2_Nabatean_Sunstone_Circle_Engraving_Floor");
       /*  slideVector = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 5f);
         startVector = this.transform.position;*/
+		invisWall = GameObject.Find ("BridgeBlock");
     }
 
 
