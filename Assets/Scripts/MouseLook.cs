c6dce3bda41e54241567aae075d801bd358f46e3
Theis Berthelsen <theis.berthelsen@gmail.com>
2015-11-30 14:36:27 +0100

    Credits and Start
    + mouselook a minor update

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -13,20 +13,20 @@ public class MouseLook : MonoBehaviour {
 
 	private float lookSensitivity = 35f;
 	private float yRotation;
-	private float xRotation;
+	//private float xRotation;
 	private float yRotationV;
-	private float xRotationV;
+	//private float xRotationV;
 	private float lookSmoothDamp = 0.1f;
 	// Needed to move player currently
 	private float currentYRotation;
-	private float currentXRotation;
+	//private float currentXRotation;
 
     private Movement3D M3;
 
     void Start()
     {
         M3 = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement3D>();
-        currentXRotation = transform.rotation.x;
+        //currentXRotation = transform.rotation.x;
         currentYRotation = transform.rotation.y;
     }
 
@@ -34,25 +34,25 @@ public class MouseLook : MonoBehaviour {
         if (M3.HasRisen)
         {
             // Getting inputs from mouse, and storing the values
-            yRotation += Input.GetAxis("Mouse X");
-            xRotation -= Input.GetAxis("Mouse Y");
+            //yRotation += Input.GetAxis("Mouse X");
+            //xRotation -= Input.GetAxis("Mouse Y");
 
             yRotation += Input.GetAxis("PS4_RightAnalogHorizontal") * lookSensitivity;
             //xRotation += Input.GetAxis("PS4_RightAnalogVertical") * lookSensitivity;
 
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f); // let the player have a 180 degree vertical view
+            //xRotation = Mathf.Clamp(xRotation, -90f, 90f); // let the player have a 180 degree vertical view
 
-            currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
+            //currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
             currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothDamp);
 
-            transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0f);
+            transform.rotation = Quaternion.Euler(0.0f, currentYRotation, 0.0f);
         }
 	}
 
-    public float CurrentXRotation
-    {
-        get { return currentXRotation; }
-    }
+    //public float CurrentXRotation
+    //{
+    //    get { return currentXRotation; }
+    //}
 
     public float CurrentYRotation
     {
